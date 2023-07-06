8026741c63fae98c577d84305126acf3cb0bbd56
millejuice <potgusdavy@gmail.com>
2023-07-07 05:00:12 +0900

    [ADD&FIX]core_music_add작업중 assets추가

--- dliveweb/lib/main.dart
+++ dliveweb/lib/main.dart
@@ -1,15 +1,28 @@
+import 'package:dliveweb/utils/host_util.dart';
 import 'package:dliveweb/web.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:dliveweb/utils/member_util.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'lib/.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider<MemberProvider>(
         create: (_) => MemberProvider(),
       ),
+      ChangeNotifierProvider<HostProvider>(
+        create: (_) => HostProvider(),
+      ),
     ],
-    child: DliveWeb(),
+    child: const DliveWeb(),
   ));
 }
